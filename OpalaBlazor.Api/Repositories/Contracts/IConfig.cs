5944774057f1b4202e12d9924db4caeec2fdd253
Inácio <104164788+ajinacios@users.noreply.github.com>
2023-01-02 11:56:58 -0400

    02012023 Exemplo de interfase

---
+++ OpalaBlazor.Api/Repositories/Contracts/IConfig.cs
@@ -0,0 +1,10 @@
+﻿using OpalaBlazor.Api.Entities;
+
+namespace OpalaBlazor.Api.Repositories.Contracts
+{
+    public interface IConfig
+    {
+        Task<IEnumerable<Config>> GetAll();
+        Task<Config> GetOne(int id);
+    }
+}
