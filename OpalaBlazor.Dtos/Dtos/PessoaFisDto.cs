688c27f1b40462a5aead22ac43ff87a26e6fcbfe
Linalaura <linalaura123@gmail.com>
2023-01-21 09:20:51 -0400

    Laura 21/01/2023 9:20

---
+++ OpalaBlazor.Dtos/Dtos/PessoaFisDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpalaBlazor.Models.Dtos
+{
+    public class PessoaFisDto
+    {
+        public int PessoaFisId { get; set; }
+
+        public string? Nome { get; set; }
+
+        public string? Login { get; set; }
+
+        public string? Senha { get; set; }
+
+        public string? Cargo { get; set; }
+
+        public string? Ativo { get; set; }
+    }
+}
---
+++ OpalaBlazor.Dtos/Dtos/ServidorDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpalaBlazor.Models.Dtos
+{
+    public class ServidorDto
+    {
+        public int ServidorId { get; set; }
+
+        public string? Nome { get; set; }
+
+        public string? Login { get; set; }
+
+        public string? Senha { get; set; }
+
+        public string? Cargo { get; set; }
+
+        public string? Ativo { get; set; }
+    }
+}
